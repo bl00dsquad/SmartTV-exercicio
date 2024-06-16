ee50300b361f9261ab0895a2d2323c3d64afa6e4
eyesley <90654931+eyesley@users.noreply.github.com>
2024-06-16 15:31:47 -0300

    Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Usuario {
+public class Usuario {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+        System.out.println("A Tv está ligada?: " + smartTv.ligada);
+        System.out.println("Em qual canal a tv está? " + smartTv.canal);
+        System.out.println("Em qual volume está a tv? " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("A tv está? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("A tv está? " + smartTv.ligada);
+
+        smartTv.ligar();
+        smartTv.aumentarVolume();
+        System.out.println("Aumento o volume da TV: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        System.out.println("Eu diminuo o volume da TV: " + smartTv.volume);
+
+        smartTv.aumentarCanal();
+        System.out.println("Você agora está assistindo o canal: " + smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("Você agora está assistindo o canal: " + smartTv.canal);
+
+        smartTv.mudarCanal(20);
+        System.out.println("Você está assistindo o canal: " + smartTv.canal);
+    }
 }
